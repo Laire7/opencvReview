a8ac351c628f6ae4eddc0ccfb16aca64ad6f9bbd
laire7 <syoungkwak@gmail.com>
2024-09-17 20:35:38 +0900

    traffic light finished

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print(f'traffic light is : {trafficLights.guess_color(random_trafficLight)}')
--- trafficLight_Class.py
+++ trafficLight_Class.py
@@ -1,5 +1,6 @@
 import cv2
-import random 
+import random
+import numpy as np 
 
 class TrafficLight: 
   trafficLight_list =  ['red', 'yellow', 'green']
@@ -51,9 +52,65 @@ class TrafficLight:
      if key == 27:  # Esc 키 누르면 종료
         cv2.destroyAllWindows()
         break
+    
+  def guess_trafficLight_color(self, img):
+    #BGR에서 HSV 색상 공간으로 변환
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # 각 색상에 대한 HSV 임계값 정의
+    red_lower1 = np.array([0, 100, 100])
+    red_upper1 = np.array([10, 255, 255])
+    red_lower2 = np.array([160, 100, 100])
+    red_upper2 = np.array([180, 255, 255])
+
+    yellow_lower = np.array([15, 100, 100])
+    yellow_upper = np.array([35, 255, 255])
+
+    green_lower = np.array([40, 50, 50])
+    green_upper = np.array([80, 255, 255])
+
+    # inRange 함수를 사용하여 각 색상 범위 내의 픽셀 마스크 생성
+    red1_mask = cv2.inRange(hsv, red_lower1, red_upper1)
+    red2_mask = cv2.inRange(hsv, red_lower2, red_upper2)
+    red_mask = cv2.bitwise_or(red1_mask, red2_mask)
+    yellow_mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+    green_mask = cv2.inRange(hsv, green_lower, green_upper)
+
+    # 각 마스크의 흰색 픽셀 수 계산
+    red_pixels = cv2.countNonZero(red_mask)
+    yellow_pixels = cv2.countNonZero(yellow_mask)
+    green_pixels = cv2.countNonZero(green_mask)
+
+    # Find contours in each mask
+    red_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    yellow_contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    green_contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+    # Find the largest contour for each color
+    largest_red_contour = None
+    largest_yellow_contour = None
+    largest_green_contour = None
+    if len(red_contours)>0:
+        largest_red_contour = max(red_contours, key=lambda c: cv2.contourArea(c), default=None)
+    if len(yellow_contours)>0:
+        largest_yellow_contour = max(yellow_contours, key=lambda c: cv2.contourArea(c), default=None)
+    if len(green_contours)>0:
+        largest_green_contour = max(green_contours, key=lambda c: cv2.contourArea(c), default=None)
+
+    # Determine the color of the traffic light based on the largest contour
+    if largest_red_contour is not None:
+      return 'red'
+    elif largest_yellow_contour is not None:
+      return 'yellow'
+    elif largest_green_contour is not None:
+      return 'green'
+    else:
+      return None
                 
     
-trafficLights = TrafficLight()
-random_trafficLight = trafficLights.get_random_trafficLight()
-trafficLights.show_pic_with_trackbar(random_trafficLight)
+new_trafficLight = TrafficLight()
+random_trafficLight = new_trafficLight.get_random_trafficLight()
+guess_color = new_trafficLight.guess_trafficLight_color(random_trafficLight)
+new_trafficLight.show_pic_with_trackbar(random_trafficLight)
+print(f'traffic light is : {guess_color}')
   
